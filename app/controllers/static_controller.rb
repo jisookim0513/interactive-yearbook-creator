e9c9082c652650917c0cdbe8951da36fc611b0a6
Pierre Karashchuk <krchtchk@gmail.com>
2015-02-07 19:10:37 -0800

    add file to jobs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,18 @@
 class StaticController < ApplicationController
-  def index
+  def new
+    @job = Job.new
+  end
+  
+  def create
+    @user = User.create( user_params )
+  end
+
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def user_params
+    params.require(:user).permit(:avatar)
   end
 end
