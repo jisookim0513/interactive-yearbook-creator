f1a859ab22c96907810fb3d47509be9db11f024b
Marion Halim <marionhalim@yahoo.com>
2015-02-08 14:05:13 -0800

    zip working

--- app/helpers/zip_helper.rb
+++ app/helpers/zip_helper.rb
@@ -7,6 +7,7 @@ module ZipHelper
 
   # Creates a zip file in the given path
   # Creates the directory if it doesn't exist
+  # Needs to be changed to take a aws urls as input and uplaods the zip file to the aws
   def zip(zipfile_path, input_filepaths)
     zipfile_name = File.basename(zipfile_path)
     directory_name = File.dirname(zipfile_path)
@@ -24,40 +25,52 @@ module ZipHelper
     return zipfile_path
   end
 
-  def process_images_from_job(zip_file, job_id)
-  	Zip::File.open(zip_name) do |zip_file|
-	# Handle entries one by one
-	zip_file.each do |entry|
-		original_file_name = entry.name
-		original_content_type = get_content_type(original_file_name)
-		original_file_size = entry.size
+  
+  #Takes a job entry, extrat it and puts individual file to the aws url
+  def process_images_from_job(job)
+    aws_url = job.file.expiring_url(10)
+    zip_path = "#{Rails.root}" + Tempfile.new('tmp').path
+    pid = system('wget -O %{dir} %{url}'  % {:dir => zip_path ,:url => aws_url})
+    tmp_dir = "#{Rails.root}" +  Dir.mktmpdir
+    Dir.mkdir(tmp_dir) unless File.exists?(tmp_dir)
+    sleep(10) #Waits until the file finishes being made and directory
 
-		watermarked_picture = watermark(original_file_name)
-		# watermarked_content_type = get_content_type(watermarked_picture)
-		# watermarked_file_name = watermarked_picture.name
-		# watermarked_file_size = watermarked.size
-		image = Image.create({ :status => 1, 
-		  		       :original_file_name => original_file_name,
-		  		       :original_content_type => original_content_type,
-		  		       :original_file_size => original_file_size,
-		  		       :original_updated_at => Time.now,
-		  		       :watermarked_file_name => "magikarp.jpg",
-		  		       :watermarked_content_type => "jpeg",
-		  		       :watermarked_file_size => 200,
-		  		       :watermarked_updated_at => Time.now,
-		  		       :job_id => job_id
-		  		     })
-		metadata = Metadata.create({
-		  			     :facebook => "magikarp",
-		  			     :linkedin => "magikarp",
-		  			   })
-
-		image.metadata_id = metadata.id
-
+    Zip::File.open(zip_path) do |zip_file|
+      # Handle entries one by one
+      zip_file.each do |entry|
+        original_file_name = entry.name
+        original_content_type = get_content_type(original_file_name)
+        original_file_size = entry.size
+        
+        watermarked_picture = watermark(original_file_name)
+        # watermarked_content_type = get_content_type(watermarked_picture)
+        # watermarked_file_name = watermarked_picture.name
+        # watermarked_file_size = watermarked.size
+        
+        entry.extract("#{Rails.root}" + tmp_dir + "/" + original_file_name)
+        image = Image.create({ :status => 1, 
+                               :original_file_name => original_file_name,
+                               :original_content_type => original_content_type,
+                               :original_file_size => original_file_size,
+                               :original_updated_at => Time.now,
+                               #:watermarked_file_name => original_file_name,
+                               #:watermarked_content_type => original_content_type,
+                               #:watermarked_file_size => original_file_size,
+                               #:watermarked_updated_at => Time.now,
+                               :job_id => job.id
+                             })
+        metadata = Metadata.create({
+                                     #:facebook => "magikarp",
+                                     #:linkedin => "magikarp",
+                                   })
+        
+        image.metadata_id = metadata.id
+        
       end
     end
+    return tmp_dir
   end
-
+  
   def get_content_type(file_name)
     temp = File.extname(file_name)
     return temp[1,temp.length]
@@ -69,6 +82,7 @@ module ZipHelper
 
   def temp()
     include ZipHelper
-    ZipHelper.process_images_from_job("marion.zip")
+    ZipHelper.process_images_from_job(Job.all[0])
+
   end
 end
