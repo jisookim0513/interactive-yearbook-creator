6fa296c824db8ebb436b78d710ef4dd66f19e336
Pierre Karashchuk <krchtchk@gmail.com>
2015-02-07 21:38:39 -0800

    annotate

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -2,10 +2,16 @@
 #
 # Table name: jobs
 #
-#  id         :integer          not null, primary key
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                :integer          not null, primary key
+#  email             :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#  file_file_name    :string(255)
+#  file_content_type :string(255)
+#  file_file_size    :integer
+#  file_updated_at   :datetime
+#  school            :text
+#  class_year        :integer
 #
 
 require 'test_helper'
